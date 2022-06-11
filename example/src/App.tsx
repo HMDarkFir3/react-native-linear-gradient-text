7be01121f14d1faec0fb3ee7b2b508739ef88580
Henrique Marques <henrique_marques.dev@outlook.com>
2022-06-10 21:27:08 -0300

    refact: dependencies

--- example/src/App.tsx
+++ example/src/App.tsx
@@ -3,13 +3,13 @@ import React from 'react';
 import { StyleSheet, View } from 'react-native';
 import { LinearGradientText } from 'react-native-linear-gradient-text';
 
-export default function App() {
+export const App = () => {
   return (
     <View style={styles.container}>
       <LinearGradientText colors={['#ff0000', '#00ff00']} text="Hello World" />
     </View>
   );
-}
+};
 
 const styles = StyleSheet.create({
   container: {
