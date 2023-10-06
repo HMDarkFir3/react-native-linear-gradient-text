ba0d701d0fcb97b7e8991a127e70a922f668ed62
Henrique Marques <henrique_marques.dev@outlook.com>
2023-10-06 14:11:57 -0300

    chore: initial commit

---
+++ README.md
@@ -0,0 +1,31 @@
+# react-native-linear-gradient-text
+
+Component to display text with linear gradient.
+
+## Installation
+
+```sh
+npm install react-native-linear-gradient-text
+```
+
+## Usage
+
+```js
+import { multiply } from 'react-native-linear-gradient-text';
+
+// ...
+
+const result = await multiply(3, 7);
+```
+
+## Contributing
+
+See the [contributing guide](CONTRIBUTING.md) to learn how to contribute to the repository and the development workflow.
+
+## License
+
+MIT
+
+---
+
+Made with [create-react-native-library](https://github.com/callstack/react-native-builder-bob)
---
+++ android/src/main/java/com/lineargradienttext/LinearGradientTextModule.java
@@ -0,0 +1,32 @@
+package com.lineargradienttext;
+
+import androidx.annotation.NonNull;
+
+import com.facebook.react.bridge.Promise;
+import com.facebook.react.bridge.ReactApplicationContext;
+import com.facebook.react.bridge.ReactContextBaseJavaModule;
+import com.facebook.react.bridge.ReactMethod;
+import com.facebook.react.module.annotations.ReactModule;
+
+@ReactModule(name = LinearGradientTextModule.NAME)
+public class LinearGradientTextModule extends ReactContextBaseJavaModule {
+  public static final String NAME = "LinearGradientText";
+
+  public LinearGradientTextModule(ReactApplicationContext reactContext) {
+    super(reactContext);
+  }
+
+  @Override
+  @NonNull
+  public String getName() {
+    return NAME;
+  }
+
+
+  // Example method
+  // See https://reactnative.dev/docs/native-modules-android
+  @ReactMethod
+  public void multiply(double a, double b, Promise promise) {
+    promise.resolve(a * b);
+  }
+}
