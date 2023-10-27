cefc5a4d56c9be6de87da2b75552177e93ebe387
Myroslav Hryschenko <miroslavgrischenko@gmail.com>
2023-10-27 17:27:31 +0300

    feat(text-props): :sparkles: added textProps to native component <Text>  for example customer can pass role/ellipsizeMode to Text and configutated it

--- src/components/LinearGradientText.tsx
+++ src/components/LinearGradientText.tsx
@@ -2,23 +2,26 @@ import React, { FC } from 'react';
 import { Text, TextStyle, StyleSheet } from 'react-native';
 import MaskedView from '@react-native-masked-view/masked-view';
 import LinearGradient from 'react-native-linear-gradient';
+import { TextProps } from 'react-native';
 
 interface Props {
   text: string;
   textStyle?: TextStyle;
+  textProps?: TextProps;
   colors: string[];
   start?: { x: number; y: number };
   end?: { x: number; y: number };
 }
 
 export const LinearGradientText: FC<Props> = (props) => {
-  const { text, textStyle = {}, colors, start = { x: 0, y: 1 }, end = { x: 1, y: 1 } } = props;
+  const { text, textStyle = {}, textProps={}, colors, start = { x: 0, y: 1 }, end = { x: 1, y: 1 } } = props;
 
   return (
     <MaskedView maskElement={<Text style={[styles.maskText, textStyle]}>{text}</Text>}>
       <LinearGradient colors={colors} start={start} end={end}>
-        <Text style={[styles.text, textStyle]}>{text}</Text>
-      </LinearGradient>
+      <Text style={[styles.text, textStyle]} {...textProps}>
+          {text}
+        </Text>      </LinearGradient>
     </MaskedView>
   );
 };
